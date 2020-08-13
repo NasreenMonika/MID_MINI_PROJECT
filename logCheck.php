1eaddb4ca52fc4c657afe94c2c571eaa8f448086
NasreenMonika <62530607+NasreenMonika@users.noreply.github.com>
2020-08-14 00:33:45 +0600

    regcheck logcheck

---
+++ logCheck.php
@@ -0,0 +1,45 @@
+<?php
+	session_start();
+
+	if(isset($_POST['LogIn'])){
+
+		$uid		= $_POST['id'];
+		$password 		= $_POST['password'];
+
+		if(empty($uid) || empty($password)){
+			echo "null submission";
+
+		}else{
+			
+			$conn = mysqli_connect('127.0.0.1', 'root', '', 'midlabexam');
+			$sql = "select * from user_info where id='".$uid."'&& password='".$password."'";
+			$result = mysqli_query($conn, $sql);
+			$user 	= mysqli_fetch_assoc($result);
+			
+			if(!empty($user))
+			{
+				
+				$_SESSION['id']  = $uid;
+				if($user['usertype']=='admin')
+				{
+					header('location: adminHome.php');
+
+				}
+				else
+				{
+				header('location: userHome.php');
+				}
+			
+				
+			}
+			else
+			{
+				echo "Invalid information";
+			}
+		}
+
+	}else{
+		header("location: login.html");
+	}
+
+?>
--- regCheck.php
+++ regCheck.php
@@ -1,52 +1,31 @@
 <?php
 
-	if(isset($_POST['submit'])){
-		$id = $_POST['id'];
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		$confirmPassword = $_POST['confirmPassword'];
-	
-
-		if( empty($password) || empty($email) || empty($name) || empty($confirmPassword) || empty($id))
-		{
-			echo "null submission".'<a href="registrattion.html"><u></a>';
-		}
-		else 
-		{
-
-			if($password!=$confirmPassword)
-			{
-				echo "Match Password".'<a href="registrattion.html"><u></a>';
-			}
-			else
-			{
-
-			$conn = mysqli_connect('127.0.0.1', 'root', '', 'miniproject');
-		$sql= 'select * from userinfo where Id="'.$_POST['id'].'"';
+	if (isset($_POST['submit'])) 
+	{
+		# code...
+		$conn = mysqli_connect('127.0.0.1', 'root', '', 'midlabexam');
+		$sql= 'select * from user_info where id="'.$_POST['id'].'"';
 		$result = mysqli_query($conn,$sql);
 		$data = mysqli_fetch_assoc($result);
 		if (empty($data)) 
 		{
-			# code...
-			if ($_POST['password'] == $_POST['confirmpassword'])
+			if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['id']) && !empty($_POST['password']) && !empty($_POST['userType']))
 			{
-				# code...
-
-				$sql1="INSERT INTO userinfo (id,name,email,password,user) VALUES ('".$_POST['id']."', '".$_POST['name']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['user']."')";
-				mysqli_query($conn,$sql1);
-				echo "done";
+				if ($_POST['password'] == $_POST['confirmPassword'])
+				{
+					$sql1="INSERT INTO user_info (id,name,email,password,usertype) VALUES ('".$_POST['id']."', '".$_POST['name']."', '".$_POST['email']."', '".$_POST['password']."', '".$_POST['userType']."')";
+					mysqli_query($conn,$sql1);
+					header("location: login.html");
+				}
 			}
 			mysqli_close($conn);
-			header('location: login.html');
-			}
 		}
-
-	}else{
-		//header("location: login.html");
-		echo "Not Set";
 	}
-
+	else
+	{
+		echo "invalid";
+	}
+	
 
 ?>
 
