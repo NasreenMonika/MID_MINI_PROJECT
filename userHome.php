706cba0df1f5969203af5ece378efcf5c5736a5a
NasreenMonika <62530607+NasreenMonika@users.noreply.github.com>
2020-08-14 12:44:08 +0600

    user home

---
+++ userHome.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title> HOME</title>
+</head>
+<body>
+
+<fieldset>
+<center><b>
+WELCOME <?php
+session_start();
+
+echo $_SESSION['name'];
+
+
+?>
+ </center></br>
+  <center>
+        <a href="profile.php"><u><b>Profile</a><br>
+        <a href="changePassword.php"><u><b>Change Password</a><br>
+        <a href="logout.php"><u><b>LOG OUT</a></br>
+    </center>
+ </fieldset>
+</body>
+</html>
